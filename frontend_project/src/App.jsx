fed799bc2f151d24de1cc9cd88bd170de1e6e4fa
Ashika-6620 <ashika20102003@gmail.com>
2025-03-18 12:42:29 +0530

    login commit

--- frontend_project/src/App.jsx
+++ frontend_project/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
+import Login from './login/login'
 
 export default function App() {
   return (
     <div>
-      <h1>Hi! I am Ashika</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos velit ipsum, natus perspiciatis ipsam, aperiam blanditiis nam beatae ut mollitia delectus est? Alias illo ipsum quibusdam eius qui adipisci quae?</p>
+      <Login/>
     </div>
   )
 }
